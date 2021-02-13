3214c80a7f348a23a2eb79eadd76a85370f668e9
mrekk-hub <maxim.shlyahtin@yandex.ru>
2021-02-13 19:33:05 +0300

    Some changes

---
+++ content.py
@@ -0,0 +1,6 @@
+content = [
+{
+        'title': 'Create your own summary.',
+        'body': 'Using our site you can easily format your articles or texts. Click below to sign up and enjoy.'
+    },
+]
