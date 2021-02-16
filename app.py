82a4eb457f7956bd767d60bcb274b7d824b91160
mrekk-hub <maxim.shlyahtin@yandex.ru>
2021-02-16 17:11:13 +0300

    Add checkout page

--- app.py
+++ app.py
@@ -10,5 +10,10 @@ def index():
     return render_template('index.html', contents=contents)
 
 
+@app.route('/checkout')
+def checkout():
+    return render_template('content.html')
+
+
 if __name__ == '__main__':
     app.run()
